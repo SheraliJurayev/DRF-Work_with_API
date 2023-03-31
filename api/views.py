650016e4a74a64e9013b63bd0b4f83b6cf29a654
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-31 20:38:28 +0500

    Custompaginations

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from api.models import Book , LibUser , RentBook
 from api.serializers import BookSerializer , LibUserSerializer , RentBookSerializer
 from rest_framework.response import Response
+from .pagination import CustomPagination
 
 
 # viewset = list ,  create , retrieve ,  update , delete ,partial_update , destroy ,
@@ -19,8 +20,8 @@ class BookViewSet(viewsets.ViewSet):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data , status=status.HTTP_201_CREATED)
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data )
+        return Response(serializer.errors )
 
 
 
@@ -30,6 +31,7 @@ class BookViewSet(viewsets.ViewSet):
 class LibUserViewSet(viewsets.ModelViewSet):
     queryset = LibUser.objects.all()
     serializer_class = LibUserSerializer
+    pagination_class = CustomPagination
 
 class RentBookViewSet(viewsets.ModelViewSet):
     queryset = RentBook.objects.all()
